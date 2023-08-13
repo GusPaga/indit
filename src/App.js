a35e978053bb4ea9c75e1b3f2948d26ffd60f029
Gustavo Pagano <gustavopagano03@gmail.com>
2023-08-13 11:54:05 +0200

    added Layout component and footer component

--- src/App.js
+++ src/App.js
@@ -7,21 +7,22 @@ import {
 import Home from './components/Pages/Home/Home';
 import DataState from './context/podCast/dataState';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from './components/Others/NavBar/NavBar.jsx';
 import { Detail } from './components/Pages/Detail/Detail';
 import { Podcast } from './components/Pages/PodCast/PodCast';
+import { Layout } from './components/Others/Layout';
 
 function App() {
 	return (
 		<Browser>
 			<DataState>
-				<NavBar />
-				<Routes>
-					<Route exact path='/' element={<Home />} />
-					<Route path='/detail/:id' element={<Detail />} />
-					<Route path='//detail/:id/podcast/:id' element={<Podcast />} />
-					<Route path='*' element={<Navigate to='/' />} />
-				</Routes>
+				<Layout>
+					<Routes>
+						<Route exact path='/' element={<Home />} />
+						<Route path='/detail/:id' element={<Detail />} />
+						<Route path='//detail/:id/podcast/:id' element={<Podcast />} />
+						<Route path='*' element={<Navigate to='/' />} />
+					</Routes>
+				</Layout>
 			</DataState>
 		</Browser>
 	);
---
+++ src/components/Others/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+export const Footer = () => {
+	return (
+		<div id='footer' className='bg-dark text-center'>
+			<div className='text-white'>
+				<ul class='nav justify-content-center'>
+					<p className='align-middle my-auto'>
+						¡This Project was made for GAP*Dev! you can see my
+					</p>
+					<li class='nav-item'>
+						<a
+							class='nav-link'
+							href='https://www.linkedin.com/in/gustavopagano03/'
+							target='_blank'
+							rel='noopener noreferrer'
+						>
+							Linkedin
+						</a>
+					</li>
+				</ul>{' '}
+			</div>
+		</div>
+	);
+};
